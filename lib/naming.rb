35513f84166462146437cd2d5356c8cb39971140
Georjane <pzgeorjane@gmail.com>
2020-07-21 21:50:20 +0100

    Add color to terminal error output

--- lib/naming.rb
+++ lib/naming.rb
@@ -1,9 +1,13 @@
+require 'colorize'
+
 class Naming
   def var_name(line, file, line_num)
     keywords = ["def", 'end', 'puts']
     if !(keywords.include?(line.split.first)) && line =~ (/ = /)
-      if !(line[(0..(line =~ (/ = /)) - 1)].split.last.match?(/^[a-z]+_[a-z]+(_[a-z]+)?$/))   
-        puts file + ":" + line_num.to_s + " use snake case to name variables"
+      regex_var = /^[a-z]+_[a-z]+(_[a-z]+)?$/
+      if !(line[(0..(line =~ (/ = /)) - 1)].split.last.match?(regex_var))
+        puts file.blue + ":" + line_num.to_s + ":" + ((line =~ (/[a-zA-Z0-9]+_[a-zA-Z0-9]+(_[a-zA-Z]+)?/)) + 1).to_s + ":" +  " C".colorize(:yellow) + ": Naming/VariableName: Use snake_case for variable names.\n  " + line[(0..(line =~ (/ = /)) - 1)].split.last
+        
       end
     end
   end
