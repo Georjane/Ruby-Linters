53f68d8a053035e5f82f639f2e83bf38a99c4e63
Georjane <pzgeorjane@gmail.com>
2020-07-24 08:23:51 +0100

    Update counting offense

--- files.rb
+++ files.rb
@@ -3,3 +3,57 @@ require_relative 'lib/naming.rb'
 require_relative 'lib/metrics.rb'
 require_relative 'lib/layout.rb'
 require_relative 'lib/colors.rb'
+
+def get_file(file)
+  line_num = 1
+  arr = []
+  File.readlines(file).each do |line|
+    name_check = Naming.new(line, file, line_num)
+    line_num += 1
+    arr << line_num if line.split.empty?
+    print name_check.var_name
+    print name_check.method_name
+    print name_check.class_name
+    print name_check.module_name
+  end
+  layout_check = Layout.new(nil, file, line_num)
+  print layout_check.empty_lines(arr)
+  get_file_metrics(file)
+end
+
+def get_file_metrics(file)
+  line_num = 0
+  def_hash = {}
+  end_hash = {}
+  metrics_check = Metrics.new(nil, file, line_num)
+  File.readlines(file).each do |line|
+    line_num += 1
+    def_hash[line_num] = line =~ /def/ if %w[def].include?(line.split.first)
+    end_hash[line_num] = line =~ /end/ if %w[end].include?(line.split.first)
+  end
+  metrics_check.block_length(def_hash, end_hash)
+end
+
+def counts(file)
+  line_num = 1
+  arr = []
+  offence_count = []
+  def_hash = {}
+  end_hash = {}
+  File.readlines(file).each do |line|
+    name_check = Naming.new(line, file, line_num)
+    line_num += 1
+    def_hash[line_num] = line =~ /def/ if %w[def].include?(line.split.first)
+    end_hash[line_num] = line =~ /end/ if %w[end].include?(line.split.first)
+    arr << line_num if line.split.empty?
+    offence_count << name_check.var_name unless name_check.var_name.nil?
+    offence_count << name_check.method_name unless name_check.method_name.nil?
+    offence_count << name_check.class_name unless name_check.class_name.nil?
+    offence_count << name_check.module_name unless name_check.module_name.nil?
+  end
+  metrics_check = Metrics.new(nil, file, line_num)
+  layout_check = Layout.new(nil, file, line_num)
+  offence_count << layout_check.empty_lines(arr) unless layout_check.empty_lines(arr).nil?
+  count = offence_count.length + metrics_check.check_length(def_hash, end_hash)
+  count
+end
--- lib/metrics.rb
+++ lib/metrics.rb
@@ -1,30 +1,43 @@
 class Metrics < Department
   def block_length(def_hash, end_hash)
-    check_length(def_hash, end_hash)
-  end
-
-  def check_length(def_hash, end_hash)
     @max = 10
-    length, final, y = Array.new(3) { [] }
-    i = 0
-    g = 0
+    length, final, many_lines = Array.new(3) { [] }
+    counter = 0
     r = 0
     end_hash.each do |_x, v|
       end_hash.delete(end_hash.key(v)) unless def_hash.value?(v)
     end
-    while i < def_hash.length
-      length << end_hash.keys[i] - def_hash.keys[i]
-      i += 1
+    while counter < def_hash.length
+      length << end_hash.keys[counter] - def_hash.keys[counter]
+      counter += 1
     end
     length.each do |a|
-      final << def_hash.keys[g] if a > @max
-      g += 1
+      final << def_hash.keys[counter] if a > @max
+      counter += 1
     end
-    length.each { |q| y << q if q > @max }
+    length.each { |q| many_lines << q if q > @max }
     while r < final.length
-      print file.blue + ':' + final[r].to_s + ':' + 1.to_s + ':' + ' C'.yellow + ': Metrics/MethodLength: Method has too many lines. [' + y[r].to_s + '/' + @max.to_s + "]\n\n"
+      print file.blue + ':' + final[r].to_s + ':' + 1.to_s + ':' + ' C'.yellow + ': Metrics/MethodLength: Method has too many lines. [' + many_lines[r].to_s + '/' + @max.to_s + "]\n\n"
       r += 1
     end
-    y.length
+  end
+
+  def check_length(def_hash, end_hash)
+    @max = 10
+    length, final, many_lines = Array.new(3) { [] }
+    counter = 0
+    end_hash.each do |_x, v|
+      end_hash.delete(end_hash.key(v)) unless def_hash.value?(v)
+    end
+    while counter < def_hash.length
+      length << end_hash.keys[counter] - def_hash.keys[counter]
+      counter += 1
+    end
+    length.each do |a|
+      final << def_hash.keys[counter] if a > @max
+      counter += 1
+    end
+    length.each { |q| many_lines << q if q > @max }
+    many_lines.length
   end
 end
