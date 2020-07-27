7dcfa16851bc0ae0d0883cb08d6772638b50c61e
Georjane <pzgeorjane@gmail.com>
2020-07-27 08:49:45 +0100

    Remove commented code in Test.rb file

--- test.rb
+++ test.rb
@@ -4,29 +4,16 @@ module MyGoodClass
   def my_Method
     my_var = "a test for processing files"
     puts 'This is ' + my_var
-    
-      # return true if my_val = "hello"
-      # my_var = "a test for processing files"
       mY_wrong23_VAR = 0
       puts 'This is ' + my_var
-    
-      # return true if my_val = "hello"r
-    
-      # return true if my_val = "hello"
-      # my_var = "a test for processing files"
     
   end
   def my_Method
     my_var = "a test for processing files"
     puts 'This is ' + my_var    
-      # return true if my_val = "hello"
-      # my_var = "a test for processing files"
       mY_wrong23_VAR = 0
         my_var = "a test for processing files"
     puts 'This is ' + my_var
-    
-      # return true if my_val = "hello"
-      # my_var = "a test for processing files"
       mY_wrong23_VAR = 0
     
   end
