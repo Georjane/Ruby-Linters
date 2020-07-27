1f81b6b5a0fcb1a966ce0950315459e18fad27c3
Georjane <pzgeorjane@gmail.com>
2020-07-27 09:15:44 +0100

    Linter can detect any ruby file in any folder

--- bin/main.rb
+++ bin/main.rb
@@ -4,15 +4,15 @@
 # !/usr/bin/env ruby
 require './lib/files.rb'
 
-def rubocop
-  num = counts('bin/bug_file/test.rb')
+def rubocop(filename)
+  num = counts(filename)
   if num.positive?
     puts "Inspecting 1 file\n\n\nOffenses:\n\n"
-    get_file('bin/bug_file/test.rb')
+    get_file(filename)
     puts '1 file inspected, ' + num.to_s.red + ' offenses'.red + ' detected'
   else
     puts "Inspecting 1 file\n" + '.'.green + "\n\n1 file inspected, " + 'no offenses'.green + ' detected'
   end
 end
 
- rubocop
+ rubocop('bin/bug_file/test.rb')
